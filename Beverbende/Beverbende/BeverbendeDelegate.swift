188141769ca1af465feb642407420dbfb66b5e59
Valkje <loran.knol@gmail.com>
2021-03-03 12:21:57 +0100

    Add BeverbendeDelegate

---
+++ Beverbende/Beverbende/BeverbendeDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  BeverbendeDelegate.swift
+//  Beverbende
+//
+//  Created by L. Knol on 03/03/2021.
+//
+
+import Foundation
+
+enum EventType {
+    case nextTurn
+}
+
+protocol BeverbendeDelegate {
+//    eventType: EventType
+//    eventInfo: [String: Any]
+    
+}
