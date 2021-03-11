ff8fa77b094426aaf6aab208137f1e0b3d63dbab
Chiel Wijs <chielwijs@live.nl>
2021-03-11 10:14:43 +0100

    added more card images

--- Beverbende/Beverbende/Beverbende.swift
+++ Beverbende/Beverbende/Beverbende.swift
@@ -26,18 +26,18 @@ class Beverbende {
         values = values + Array(repeating: 7, count: 4)
         values = values + Array(repeating: 8, count: 4)
         values = values + Array(repeating: 9, count: 9)
-//        let actions = []
-//            Array(repeating: Action.inspect, count: 7) +
-//            Array(repeating: Action.twice, count: 5) +
-//            Array(repeating: Action.swap, count: 9)
+        let actions = []
+            Array(repeating: Action.inspect, count: 7) +
+            Array(repeating: Action.twice, count: 5) +
+            Array(repeating: Action.swap, count: 9)
         
         var cards: [Card] = []
         for v in values {
             cards.append(ValueCard(value: v))
         }
-//        for a in actions {
-//            cards.append(ActionCard(value: a))
-//        }
+        for a in actions {
+            cards.append(ActionCard(value: a))
+        }
         
         return cards
     }
